7a8ee9d2cb133048d8e0619d5dff8150e6e45259
“kanmani <arunachalamalagappan7@gmail.com>
2021-10-03 10:24:01 +0530

    Making changes to print statements

--- WebhooksExample.java
+++ WebhooksExample.java
@@ -3,6 +3,7 @@ public class WebhooksExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Hello Web Hooks!!!");
+       System.out.println("Adding a new sysout");
 	}
 
 }
