740f05ed56a8297aa3202e9e901ded7ae26f12ac
“kanmani <arunachalamalagappan7@gmail.com>
2021-10-03 11:34:12 +0530

    Removing web hooks example new file

--- WebhooksExampleNewFile.java
+++
@@ -1,9 +0,0 @@
-public class WebhooksExampleNewFile {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       System.out.println("Hello Web Hooks!!!");
-       System.out.println("Adding a new sysout");//Adding a comment
-	}
-
-}
