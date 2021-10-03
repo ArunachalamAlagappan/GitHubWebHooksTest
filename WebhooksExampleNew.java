9d745312faa5501aff31c7bb5ffd65f82ac719d2
“kanmani <arunachalamalagappan7@gmail.com>
2021-10-03 21:38:12 +0530

    Changing web hooks file in Master

--- WebhooksExampleNew.java
+++ WebhooksExampleNew.java
@@ -2,7 +2,7 @@ public class WebhooksExampleNew {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("Hello Web Hooks New!!!");//Test
+       System.out.println("Hello Web Hooks New!!!");//In Master
 	}
 
 }
